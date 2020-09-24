2ebe3e6e3d4dc6f4da5372d1d143cfea9d43b399
Scott G <scottg489@gmail.com>
2020-09-24 13:00:11 -0400

    Don't show email subscription signup
    - Removed from the nav bar
    - Removed from the bottom of posts

--- src/website-config.ts
+++ src/website-config.ts
@@ -62,7 +62,7 @@ const config: WebsiteConfig = {
   github: 'https://github.com/ScottG489',
   twitter: 'https://twitter.com/scottg489',
   facebook: 'https://www.facebook.com/scott.giminiani',
-  showSubscribe: true,
+  showSubscribe: false,
   mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
