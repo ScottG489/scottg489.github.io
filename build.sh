75dbebb1cc7c20f71ac8495b2ebc4b020b15664e
Scott Giminiani <scottg489@gmail.com>
2021-02-09 20:44:03 -0800

    Use new job runner for build
    simple-ci.com is where the legacy job runner is hosted. conjob.io is
    the new server.
    
    - Make build script more readable

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
-curl -v -sS -w '%{http_code}' \
-  --data-binary '{"ID_RSA": "'"$1"'", "AWS_CREDENTIALS": "'"$2"'"}' \
-  'http://api.simple-ci.com/build?image=scottg489/scottg489.github.io-build:latest' \
+#!/usr/bin/env bash
+
+readonly IMAGE_NAME='scottg489/scottg489.github.io-build:latest'
+readonly ID_RSA=$1
+readonly AWS_CREDENTIALS=$2
+
+read -r -d '' JSON_BODY <<- EOM
+  {
+  "ID_RSA": "$ID_RSA",
+  "AWS_CREDENTIALS": "$AWS_CREDENTIALS"
+  }
+EOM
+
+curl -v -sS -w '\n%{http_code}' \
+  --data-binary "$JSON_BODY" \
+  "http://api.conjob.io/job/run?image=$IMAGE_NAME" \
   | tee /tmp/foo \
   | sed '$d' && \
   [ "$(tail -1 /tmp/foo)" -eq 200 ]
