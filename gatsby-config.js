709f44527580f24f69d7b3036dc214cc900980c3
Scott Giminiani <scottg489@gmail.com>
2022-02-28 01:09:45 -0500

    Add Google Analytics tracking ID

--- gatsby-config.js
+++ gatsby-config.js
@@ -80,7 +80,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-221469674-1',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
