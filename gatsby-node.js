ff8207c87bdec367c2e55266d0d20ef407ad5749
Scott Giminiani <scottg489@gmail.com>
2022-12-08 21:58:32 -0500

    Allow draft posts to be navigated to directly
    Draft posts will continue to not be visible on the home page, posts
    page, or anywhere on the site. However, if the post is navigated to
    directly, for instance if I tell someone it, then it will be visible.

--- gatsby-node.js
+++ gatsby-node.js
@@ -102,7 +102,7 @@ exports.createPages = async ({ graphql, actions }) => {
       allMarkdownRemark(
         limit: 2000
         sort: { fields: [frontmatter___date], order: ASC }
-        filter: { frontmatter: { draft: { ne: true } }, fileAbsolutePath: {regex: "/content/posts/"} }
+        filter: { fileAbsolutePath: {regex: "/content/posts/"} }
       ) {
         edges {
           node {
